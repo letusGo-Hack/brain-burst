30de3c1c6f8510b9eefda7cd72969e5da389b3ea
ebpark9511 <ebpark9511@gmail.com>
2023-06-10 15:24:39 +0900

    feat: AnswerNotificationToast 추가

--- BrainBurst/BrainBurst/Common/Utils/Utils.swift
+++ BrainBurst/BrainBurst/Common/Utils/Utils.swift
@@ -6,3 +6,35 @@
 //
 
 import Foundation
+import SwiftUI
+
+struct IsVisibleModifier : ViewModifier{
+   
+   var isVisible : Bool
+   var transition : AnyTransition
+   
+   func body(content: Content) -> some View {
+       ZStack{
+           if isVisible {
+               content.transition(transition)
+           }
+       }
+   }
+}
+
+extension View {
+   
+   func isVisible(
+       isVisible : Bool,
+       transition : AnyTransition = .scale
+   ) -> some View{
+       modifier(
+           IsVisibleModifier(
+               isVisible: isVisible,
+               transition: transition
+           )
+       )
+       
+       
+   }
+}
