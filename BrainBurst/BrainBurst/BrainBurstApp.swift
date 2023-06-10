9878bb4acede355c6309b54a78dcb37f9576ceb5
jmindeveloper <jmin.developer@gmail.com>
2023-06-10 13:22:10 +0900

    BrainBurst 프로젝트 생성

---
+++ BrainBurst/BrainBurst/BrainBurstApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrainBurstApp.swift
+//  BrainBurst
+//
+//  Created by J_Min on 2023/06/10.
+//
+
+import SwiftUI
+
+@main
+struct BrainBurstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
