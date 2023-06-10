755590062a45ef4727e36855d1b4c130e071fc9e
고대원 <kodaewon@ip-192-168-222-193.ap-northeast-2.compute.internal>
2023-06-10 16:23:15 +0900

    게임 결과 화면 올라오면 GameResultHistory 저장하는 로직 추가

--- BrainBurst/BrainBurst/Views/Rank/RankView.swift
+++ BrainBurst/BrainBurst/Views/Rank/RankView.swift
@@ -6,9 +6,13 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct RankView: View {
     @Environment(\.dismiss) var dismiss
+    @Environment(\.modelContext) private var context
+    
+    @State private var didLoad: Bool = false
     
     private var histories: [Int] = [1, 2, 3, 4, 5]
     
@@ -29,6 +33,21 @@ struct RankView: View {
                 }
             })
         })
+        .onAppear(perform: {
+            if didLoad == false {
+                didLoad = true
+            }
+            
+            let gameResultHisotry = GameResultHistory(
+                rank: 1,
+                score: 10,
+                endDate: Date(),
+                createDate: Date()
+            )
+            context.insert(gameResultHisotry)
+            try? context.save()
+        })
+        
     }
     
     func historyView(row: Int) -> some View {
@@ -51,6 +70,7 @@ struct RankView: View {
             isPresented: Binding.constant(true),
             content: {
                 RankView()
+                    .modelContainer(for: GameResultHistory.self)
             }
         )
 }
