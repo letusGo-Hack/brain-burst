441dc0a109b02ae8626d3e0a9a9867f8c2e2a9a2
고대원 <kodaewon@ip-192-168-222-193.ap-northeast-2.compute.internal>
2023-06-10 14:28:29 +0900

    feat : 랭크 화면 리스트 형태로 기록 나오게 개발

--- BrainBurst/BrainBurst/Rank/RankView.swift
+++ BrainBurst/BrainBurst/Rank/RankView.swift
@@ -8,18 +8,40 @@
 import SwiftUI
 
 struct RankView: View {
+    @Environment(\.dismiss) var dismiss
+    
+    private var histories: [Int] = [1, 2, 3, 4, 5]
+    
     var body: some View {
         NavigationView(content: {
-            Text("Hi")
-                .navigationBarTitleDisplayMode(.inline)
-                .navigationBarTitle("Navigation")
-                .toolbar(content: {
-                    ToolbarItem(placement: .topBarLeading) {
-                            Button("One") {}
-                          }
-                })
-            
+            List {
+                ForEach(histories, id: \.self) { history in
+                    historyView(row: history)
+                }
+            }
+            .navigationBarTitleDisplayMode(.inline)
+            .navigationBarTitle("Rank")
+            .toolbar(content: {
+                ToolbarItem(placement: .topBarLeading) {
+                    Button("Close") {
+                        dismiss()
+                    }
+                }
+            })
         })
+    }
+    
+    func historyView(row: Int) -> some View {
+        HStack {
+            Text("\(row)")
+                .frame(maxWidth: .infinity)
+            
+            Text("\(row)")
+                .frame(maxWidth: .infinity)
+            
+            Text("\(row)")
+                .frame(maxWidth: .infinity)
+        }
     }
 }
 
