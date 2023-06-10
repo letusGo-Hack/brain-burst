4bf3d0577b8a41bd27c2a821a013cdca48ce4417
jmindeveloper <jmin.developer@gmail.com>
2023-06-10 14:52:24 +0900

    gameManager 원하는 개수 퀴즈생성

---
+++ BrainBurst/BrainBurst/Game/GameManager.swift
@@ -0,0 +1,26 @@
+//
+//  GameManager.swift
+//  BrainBurst
+//
+//  Created by J_Min on 2023/06/10.
+//
+
+import Foundation
+
+final class GameManager {
+    
+    private var quizMaker: QuizMaker
+    
+    init(maker: QuizMaker) {
+        self.quizMaker = maker
+    }
+    
+    func makeQuiz(_ count: Int) -> [Quiz] {
+        var quizs: [Quiz] = []
+        for _ in 0..<count {
+            let quiz = quizMaker.makeQuiz()
+            quizs.append(quiz)
+        }
+        return quizs
+    }
+}
